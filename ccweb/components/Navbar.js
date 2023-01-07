31d38af7cdbfbaeafe0b24d4d6be13243503a34c
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-01-07 11:29:01 +0530

    next js setup

---
+++ ccweb/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="text-center text-3xl font-light text-green-800">
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
---
+++ ccweb/pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-6xl font-extralight text-center">
+      <h1 className="text-green-800">CropConnect Website</h1>
+    </div>
+  )
+}
